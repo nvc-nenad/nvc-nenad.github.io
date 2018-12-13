34c132447b70d7239aa1bfb0235158ab85ad8a03
Nenad Manoilov <nmanoilov@alamo.edu>
2018-12-12 18:45:46 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('#btn-animate').on('click',function() {
+
+   
+    // $('#btn-animate').addClass('animated fadeOutLeft');
+    $('#title').fadeOut(500);
+  })
+
+
+
+  setTimeout(function() {
+    // $('.hero-title').fadeOut(200);
+    // $('#title').addClass('animated fadeOutRight');
+  }, 500);
+
+
+
+});
